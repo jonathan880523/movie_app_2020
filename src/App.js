914287228ad4d179c86e02cdec614918bd954b75
jonathan880523 <seo880523@gmail.com>
2020-06-04 17:40:55 +0900

    completed movie lists

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function Food({ name }) {
-  return <h1>I like {name}</h1>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              console.log(movie);
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
-
-const foodIlike = [
-  {
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkembap_02.jpg",
-  },
-  {
-    name: "Samgyeopsal",
-    image:
-      "https://post-phinf.pstatic.net/MjAyMDAzMDNfMTcg/MDAxNTgzMTkwNjA3ODQ5.kUXPHqGJ2xPDSu_3FiEoFC3kY9QyQ_g9CziCGrFSDuEg.LpCfOTbc5qth9d-GKzGv9jwj2VKhcqmPHp5cp1KJYEsg.JPEG/IM_food02.jpg?type=w1200",
-  },
-  {
-    name: "Bibimbap",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/2880px-Dolsot-bibimbap.jpg",
-  },
-  {
-    name: "Doncasu",
-    image:
-      "http://th2.tmon.kr/thumbs/image/423/336/e17/1b1492318_700x700_95_FIT.jpg",
-  },
-  {
-    name: "kimbap",
-    image: "https://craftlog.com/m/i/5805364=s1280=h960.webp",
-  },
-];
-
-function App() {
-  return (
-    <div>
-      {foodIlike.map((dish) => (
-        <Food name={dish.name} />
-      ))}
-    </div>
-  );
-}
-
 export default App;
